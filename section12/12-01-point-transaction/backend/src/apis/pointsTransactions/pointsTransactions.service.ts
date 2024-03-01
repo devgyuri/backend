9f15abcdcee841b5a236732292256b9585ccc847
devgyuri <gksmf910@gmail.com>
2024-03-01 16:52:26 +0900

    point transaction API

---
+++ section12/12-01-point-transaction/backend/src/apis/pointsTransactions/pointsTransactions.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {
+  POINT_TRANSACTION_STATUS_ENUM,
+  PointTransaction,
+} from './entities/pointTransaction.entity';
+import { IPointsTransactionsServiceCreate } from './interfaces/points-transactions-service.interface';
+import { User } from '../users/entities/user.entity';
+
+@Injectable()
+export class PointsTransactionsService {
+  constructor(
+    @InjectRepository(PointTransaction)
+    private readonly pointsTransactionsRepository: Repository<PointTransaction>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create({
+    impUid,
+    amount,
+    user: _user,
+  }: IPointsTransactionsServiceCreate): Promise<PointTransaction> {
+    const pointTransaction = this.pointsTransactionsRepository.create({
+      impUid,
+      amount,
+      user: _user,
+      status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+    });
+    await this.pointsTransactionsRepository.save(pointTransaction);
+
+    const user = await this.usersRepository.findOne({
+      where: { id: _user.id },
+    });
+
+    await this.usersRepository.update(
+      { id: _user.id },
+      { point: user.point + amount },
+    );
+
+    return pointTransaction;
+  }
+}
