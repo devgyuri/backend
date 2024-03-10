f8aae3e24e48fbb8f2fd16548ec9112488642531
devgyuri <gksmf910@gmail.com>
2024-03-10 23:17:18 +0900

    naver login with error

---
+++ section11/11-07-social-login/backend/src/apis/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { IOAuthUser } from './interfaces/auth-service.interface';
+
+@Controller()
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService, //
+  ) {}
+
+  @UseGuards(AuthGuard('google'))
+  @Get('/login/google')
+  async loginGoogle(
+    @Req() req: Request & IOAuthUser, //
+    @Res() res: Response,
+  ) {
+    return this.authService.loginOAuth({ req, res });
+  }
+
+  @UseGuards(AuthGuard('naver'))
+  @Get('/login/naver')
+  async loginNaver(
+    @Req() req: Request & IOAuthUser, //
+    @Res() res: Response,
+  ) {
+    return this.authService.loginOAuth({ req, res });
+  }
+}
